82333c42cda24610214a9f64c1f6fc73013eb803
joepound <14357797+joepound@users.noreply.github.com>
2019-07-02 11:36:34 -0600

    Fix status typo for endpoint '/api/users' and add more documentation

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,12 +2,15 @@ const route = require("express").Router();
 const { authenticate } = require("../../common/authentication");
 const models = require("../../common/helpers");
 
+// @route    /api/users
+// @desc     GET current user
+// @Access   Private
 route.get("/", authenticate, async (req, res) => {
   const { id } = req.decoded;
 
   try {
     const userInfo = await models.findBy("users", { id });
-    res.status(500).json(userInfo);
+    res.status(200).json(userInfo);
   } catch ({ message }) {
     res.status(500).json({ message });
   }
