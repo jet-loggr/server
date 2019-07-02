88cc62517186cbb6cd1ebed0fbec60c60c14683a
joepound <14357797+joepound@users.noreply.github.com>
2019-07-02 12:22:59 -0600

    Add user update endpoint

--- routes/users/index.js
+++ routes/users/index.js
@@ -16,4 +16,15 @@ route.get("/", authenticate, async (req, res) => {
   }
 });
 
+route.put("/", async (req, res) => {
+  const { id } = req.decoded;
+
+  try {
+    await models.update("users", { id }, { ...req.body });
+    res.status(200).json({ message: "User update successful." });
+  } catch ({ message }) {
+    res.status(500).json({ message });
+  }
+});
+
 module.exports = route;
